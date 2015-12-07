ba383b2e061d36b43e3624041587b1f980353278
Will Duffy <willmanduffy@gmail.com>
2015-12-07 12:07:27 -0500

    fuck commit messages this is the real world

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 $(function() {
-  const PARENT_FACE_WIDTH = 135;
+  const PARENT_FACE_WIDTH = 135; // The width of the face hole. Gross.
 
-  var imageFace = $('#image-face');
+  var imageFace = $('#image-face'); // The element the face is planted onto
   var imagePreview = $('#image-preview');
 
   $('#image-file-field').change(function() {
@@ -16,6 +16,8 @@ $(function() {
     processFace();
   });
 
+  // Return how big the original face is in comparision with the "face hole".
+  // Again, gross.
   function findScale(face) {
     return (PARENT_FACE_WIDTH / face.width);
   }
@@ -26,10 +28,11 @@ $(function() {
     imageFace.attr('src', imagePath);
   }
 
+  // This is where the face detection / image "validation" is done.
   function processFace() {
     imagePreview.faceDetection({
       complete: function(faces) {
-        var face = faces[0];
+        var face = randomFace(faces);
 
         if (face) {
           placeFace();
@@ -41,27 +44,46 @@ $(function() {
     });
   }
 
+  // I haven't gone ahead and implemented a way to actually target a face so I'm
+  // just picking a random face that's detected because I hate humanity.
+  function randomFace(faces) {
+    return $.rand(faces);
+  }
+
+  // This sets a preview field that's used for facial detection. A future
+  // refactor could probably remove this step altogether and do everything on
+  // the fly but that's a step for another day y'all.
   function showPreview(data) {
     var reader = new FileReader();
 
     reader.onload = function(event) {
-      imagePreview.
-        attr('src', event.target.result).
-        height(300);
+      imagePreview.attr('src', event.target.result)
     }
 
     reader.readAsDataURL(data);
   }
 
+  // Actually size the face behind the main image
   function sizeFace(face) {
     var scale = findScale(face);
+    var marginLeft = face.x * scale;
+    var marginTop = face.y * scale;
 
-    console.log(scale);
-
-    // This math literally does not work yet
     imageFace.
-      css('margin-left', -(face.x)).
-      css('margin-top', -(face.y / (scale * 1.5))).
+      css('margin-left', -(marginLeft)).
+      css('margin-top', -(marginTop)).
       css('transform', 'scale(' + scale + ')');
   }
+
+  // Let's make a random function that we're stealing from Stack Overflow
+  // because my time is worth something to me and you don't know me.
+  $.rand = function(arg) {
+    if ($.isArray(arg)) {
+      return arg[$.rand(arg.length)];
+    } else if (typeof arg === "number") {
+      return Math.floor(Math.random() * arg);
+    } else {
+      return 4;  // chosen by fair dice roll
+    }
+  };
 });
